146c60c8b76b6508705ba63ea732ced45a0c7d27
mervecelik11 <115151241+mervecelik11@users.noreply.github.com>
2022-12-22 17:04:10 +0300

    Update sw.js

--- Music-Player-App/sw.js
+++ Music-Player-App/sw.js
@@ -8,3 +8,13 @@ function installCB(e) {
   )
 }
 self.addEventListener('install', installCB)
+
+function cacheCB(e) { //cache first
+  let req = e.request
+  e.respondWith(
+    caches.match(req)
+    .then(r1 => r1 || fetch(req))
+    .catch(console.log)
+  )
+}
+self.addEventListener('fetch', cacheCB)
